62ebe0c8a6a4a801e5f49ead80df9db57c73a92b
Grace <mgracebodur@gmail.com>
2019-12-17 02:51:55 -0500

    Finished drills

--- sort.js
+++ sort.js
@@ -1,12 +1,26 @@
 function sort(list) {
-    for(let i = 2; i < list.length; i++){
-      let j = i;
-      while(j > 0 && list[j - 1] > list[j]){
-        let temp = list[j];
-        list[j] = list[j - 1];
-        list[j - 1] = temp;
-        j--;
-      }
-    }
-    return list;
-  }
+  return list.sort((a, b) => {
+      return a - b; 
+   });
+ }
+
+ module.exports = sort;
+
+
+
+
+
+// function sort(list) {
+//     for(let i = 2; i < list.length; i++){
+//       let j = i;
+//       while(j > 0 && list[j - 1] > list[j]){
+//         let temp = list[j];
+//         list[j] = list[j - 1];
+//         list[j - 1] = temp;
+//         j++;
+//       }
+//     }
+//     return list;
+//   }
+
+//   module.exports = sort
